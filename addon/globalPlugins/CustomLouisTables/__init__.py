9c1977a428f55ebd319204c529fc68ad94a0bf3b
Tseng Woody <tsengwoody.tw@gmail.com>
2018-04-12 20:54:00 +0800

    CustomLouisTables initial commit

---
+++ addon/globalPlugins/CustomLouisTables/__init__.py
@@ -0,0 +1,61 @@
+﻿# coding: utf-8
+# CustomLouisTables:
+# Copyright (C) 2018 Tseng Woody <tsengwoody.tw@gmail.com>
+# This file is covered by the GNU General Public License.
+# See the file COPYING.txt for more details.
+
+import inspect
+import os
+import shutil
+import sys
+
+
+import config
+import globalPluginHandler
+import globalVars
+import ui
+import braille
+import braille_custom
+
+origin_regions = {i: getattr(braille, i) for i in dir(braille) if inspect.isclass(getattr(braille, i)) and issubclass(getattr(braille, i), braille.Region) }
+custom_regions = {i: getattr(braille_custom, i) for i in dir(braille_custom) if inspect.isclass(getattr(braille_custom, i)) and issubclass(getattr(braille_custom, i), braille_custom.Region) }
+
+class GlobalPlugin(globalPluginHandler.GlobalPlugin):
+	scriptCategory = _("CustomLouisTables")
+
+	def __init__(self, *args, **kwargs):
+		super(GlobalPlugin, self).__init__(*args, **kwargs)
+
+		try:
+			config.conf["CustomLouisTables"]["toggle"]
+		except:
+			config.conf["CustomLouisTables"] = {}
+			config.conf["CustomLouisTables"]["toggle"] = "On"
+
+		if config.conf["CustomLouisTables"]["toggle"] == "On":
+			for key, value in custom_regions.items():
+				setattr(braille, key, value)
+		else:
+			for key, value in origin_regions.items():
+				setattr(braille, key, value)
+
+	def script_toggle(self, gesture):
+
+		try:
+			config.conf["CustomLouisTables"]["toggle"]
+		except:
+			config.conf["CustomLouisTables"] = {}
+			config.conf["CustomLouisTables"]["toggle"] = "On"
+
+		if config.conf["CustomLouisTables"]["toggle"] == "On":
+			for key, value in origin_regions.items():
+				setattr(braille, key, value)
+			config.conf["CustomLouisTables"]["toggle"] = "Off"
+		else:
+			for key, value in custom_regions.items():
+				setattr(braille, key, value)
+			config.conf["CustomLouisTables"]["toggle"] = "On"
+		ui.message(config.conf["CustomLouisTables"]["toggle"])
+	script_toggle.category = scriptCategory
+	# Translators: message presented in input mode.
+	script_toggle.__doc__ = _("Toggle CustomLouisTables")
---
+++ readme.md
@@ -0,0 +1,9 @@
+# CustomLouisTables 說明
+
+此附加元件提供自訂轉譯表功能，
+當 louis/tables 內有檔名為：「custom_原先轉譯表」時，會優先使用該轉譯表
+例如：點字設定內轉譯表為「中文（台灣）注音」(zh-tw.ctb)時
+且 louis/tables 內有 custom_zh-tw.ctb則會優先使用此轉譯表
+是否優先使用自訂轉譯表亦可選擇是否啟用
+請先至「輸入手勢->CustomLouisTables->Toggle CustomLouisTables」設定快速鍵
+完成後即可使用快速鍵開關此功能
